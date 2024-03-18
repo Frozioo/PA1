df59216f70506bec05a79998e7ec2dd948b30230
Frozio <tml100@live.com>
2024-03-17 23:27:11 -0500

    should be finished

---
+++ PartA.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+     // declared variables
+     FILE *inputFile;
+     FILE *outputFile;
+     FILE *print;
+     char integers;
+     int numbers;
+
+     inputFile = fopen(argv[1], "r");
+
+
+     // argument checks
+
+	if (argv[1] == NULL) {
+		printf("Please provide the input and output text file names as: %s name1.txt name2.txt\n", argv[0]);
+		exit(0);
+	}
+
+	if (argv[2] == NULL) {
+		printf("Please provide the input and output text file names as: %s name1.txt name2.txt\n", argv[0]);
+		exit(0);
+	}
+     // if more than three file arguments
+     if (argv[3] != NULL) {
+          printf("Please don't put more than two files!");
+          exit(0);
+     }
+
+     if (inputFile == NULL) {
+     printf("Input file %s cannot be opened.\n", argv[1]);
+     printf("Please create the input file!");
+     exit(0);
+     }
+     
+     // printing out the integers in the inputFile
+     print = fopen(argv[1], "r");
+     printf("The integers in file %s:\n", argv[1]);
+
+     while((integers = fgetc(print)) != EOF) {
+          printf("%c", integers);
+     }
+
+     // creation of the output file
+     outputFile = fopen(argv[2], "w");
+     printf("\n");
+     printf("The integers in file %s:\n", argv[2]);
+     
+     do {
+          numbers = fgetc(inputFile);
+          if(feof(inputFile)) {
+               break;
+          }
+          printf("%c", numbers);
+          fputc(numbers, outputFile);
+     } while(1);
+     
+     // can't forget to close opened files
+     fclose(inputFile);
+     fclose(outputFile);
+     fclose(print);
+
+     return 0;
+}
---
+++ PartB.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *xp, int *yp);
+
+int main(int argc, char* argv[]) {
+     
+     FILE* inputFile; 
+     FILE* outputFile; 
+     char integers;
+    
+     inputFile = fopen(argv[1], "r");
+     
+     // argument checks
+
+	if (argv[1] == NULL) {
+		printf("Please provide the input and output text file names as: %s name1.txt name2.txt\n", argv[0]);
+		exit(0);
+	}
+
+	if (argv[2] == NULL) {
+		printf("Please provide the input and output text file names as: %s name1.txt name2.txt\n", argv[0]);
+		exit(0);
+	}
+     // if more than three file arguments
+     if (argv[3] != NULL) {
+          printf("Please don't put more than two files!");
+          exit(0);
+     }
+
+     if (inputFile == NULL) {
+     printf("Input file %s cannot be opened.\n", argv[1]);
+     printf("Please create the input file!");
+     exit(0);
+     }
+
+     int* arr = NULL;
+     int index = 0;
+    
+     while (fscanf(inputFile, "%d", &integers) == 1) {
+        arr = (int*)realloc(arr, (index + 1) * sizeof(int));
+        arr[index++] = integers;
+     }
+
+     fclose(inputFile);
+
+     // Sort the array
+     for (int i = 0; i < index - 1; i++) {
+        for (int j = i + 1; j < index; j++) {
+          if (arr[i] > arr[j]) {
+               swap(&arr[i], &arr[j]);
+          }
+        }
+     }
+
+     // Print the sorted array
+     printf("The integers in file %s after sorting:\n", argv[1]);
+     for (int i = 0; i < index; i++) {
+        printf("%d", arr[i]);
+        if (i != index - 1) {
+          printf(" ");
+        }
+     }
+     printf("\n");
+
+     outputFile = fopen(argv[2], "w");
+
+     // Write the sorted array to the output inputFile
+     for (int i = 0; i < index; i++) {
+        fprintf(outputFile, "%d", arr[i]);
+        if (i != index - 1) {
+          fprintf(outputFile, " ");
+        }
+     }
+
+     fclose(outputFile);
+     free(arr);
+    
+     return 0;
+}
+
+void swap(int *xp, int *yp) {
+     int temp = *xp;
+     *xp = *yp;
+     *yp = temp;
+}
